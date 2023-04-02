10021a52f8afc61fcc066ae83ac3ca537cf97dbb
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-04-03 00:59:56 +0600

    added header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100 lg:container mx-auto">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl lg:text-3xl">Best Players</a>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img className='h-16 w-16 rounded-full' src='https://images.unsplash.com/photo-1677856217095-65c6e0e4bd70?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80' />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
